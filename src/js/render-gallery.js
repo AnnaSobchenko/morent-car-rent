a805412df2bd2366c62bcb4b1974063db18bf63a
AnnaSobchenko <annsbchnk@gmail.com>
2022-08-19 23:26:58 +0300

    add card

---
+++ src/js/render-gallery.js
@@ -0,0 +1,18 @@
+// const Car=require("./db/cars.json")
+
+//  <div class="gallery">
+// <div class="card__large">
+//     <picture class="image">
+//         <source
+//           srcset="./images/user-mob-1x.png 1x, ./images/user-mob-2x.png 2x"
+//           media="(max-width: 767px)" />
+//           <source
+//     srcset="./images/user-desk-1x.png 1x, ./images/user-desk-2x.png 2x"
+//     media="(min-width: 768px)" />
+//         <img src="./images/user-mob-1x.png" alt="profile-user"
+//       /></picture>
+//     <div class="card__views">
+
+//     </div>
+// </div>
+// </div> 
