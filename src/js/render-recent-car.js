5c3ff05349dd70d0a4abacdcf19a6d41ea937e10
AnnaSobchenko <annsbchnk@gmail.com>
2022-08-20 14:06:02 +0300

    update

---
+++ src/js/render-recent-car.js
@@ -0,0 +1,9 @@
+const cardEL = document.querySelector('.recent-car__list');
+
+function renderRecentCars  (info)  {
+     return ( 
+    cardEL.innerHTML=<div class="render">card</div>
+     );
+}
+ 
+export default renderRecentCars;
