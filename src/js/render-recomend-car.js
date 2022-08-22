3b61f33119f5d66a2658724e15725e472b420945
AnnaSobchenko <annsbchnk@gmail.com>
2022-08-22 16:05:59 +0300

    swipe add

--- src/js/render-recomend-car.js
+++ src/js/render-recomend-car.js
@@ -1,6 +1,5 @@
 import cars from '../db/cars.json';
 import icons from '../images/symbol-defs.svg';
-import {imagescars} from './root.js'
 import imagecrv from '../images/cars/cr-v/cr-v-desk-1x-min.png'
 import imagenissan from '../images/cars/nissan/nissan-gt-r-desk-1x-min.png'
 import imageKoenigsegg from '../images/cars/koenigsegg/koenigsegg-large-desk-1x-min.png'
@@ -8,12 +7,12 @@ import imagerush from '../images/cars/rush/rush-large-desk-1x-min.png'
 import imageterios from '../images/cars/terios/terios-large-desk-1x-min.png'
 import imagerolls from '../images/cars/rolls/rolls-royce-large-desk-1x-min.png'
 
-const {desk1nissan}=imagescars;
-const cardEL = document.querySelector('.recent-car__list');
+const cardRecomEL = document.querySelector('.recomendation-car__list');
 
-const cards = cars.map(card => {
+const recomendation=cars.filter(el=>el.favorite===true)
+
+const cards = recomendation.map(card => {
   let model = '';
-  let imagesrc=""
   switch (card.name) {
     case 'Nissan GT-R':
       model = 'nissan';
@@ -41,13 +40,21 @@ const cards = cars.map(card => {
       break;
 
     }
+
+//   let image = document.createElement("img");
+// image.src = `http://localhost:1234/${card.images.desk1}`;
+// image.alt =`${card.name}`;
+// console.log(image);
+
+// cardRecomEL.append(image);
+
+
   return `<li class="model swiper-slide" key=${card.id} id=${card.id}>
   <h3>${card.name}</h3>
   <p class="card-type">${card.type}</p>
-    
-    <img src=${imagesrc} alt="car" />
-    
-    
+  
+  <img src=${imagesrc} alt="car" />
+  </div>
   <div class="options-svg">
     <div class="info">
       <svg>
@@ -65,9 +72,9 @@ const cards = cars.map(card => {
       <svg>
         <use href="${icons}#icon-profile-2user"></use>
       </svg>
-      <p>${card.capacity}&nbsp;People</p>
-    </div></div>
+      <p>${card.capacity}&nbsp;People</p>    
   </div>
 </li>`
 }).join('');
-cardEL.innerHTML = cards;
+
+cardRecomEL.innerHTML= cards;
---
+++ src/js/root.js
@@ -0,0 +1,38 @@
+export const imagescars = {
+  desk1nissan: '../images/cars/nissan/nissan-gt-r-desk-1x-min.png',
+  desk2nissan: '../images/cars/nissan/nissan-gt-r-desk-2x-min.png',
+  large1desknissan: '../images/cars/nissan/nissan-large-desk-1x-min.png',
+  large2desknissan: '../images/cars/nissan/nissan-large-desk-2x-min.png',
+  large1mobnissan: '../images/cars/nissan/nissan-large-mob-1x-min.png',
+  large2mobnissan: '../images/cars/nissan/nissan-large-mob-2x-min.png',
+  viewdesk1nissan: '../images/cars/nissan/nissan-view-desk-1x-min.png',
+  viewdesk2nissan: '../images/cars/nissan/nissan-view-desk-2x-min.png',
+  viewmob1nissan: '../images/cars/nissan/nissan-view-mob-1x-min.png',
+  viewmob2nissan: '../images/cars/nissan/nissan-view-mob-2x-min.png',
+  largeview2desk1nissan:
+    '../images/cars/nissan/nissan-large-view2-desk-1x-min.png',
+  largeview2desk2nissan:
+    '../images/cars/nissan/nissan-large-view2-desk-2x-min.png',
+  largeview2mob1nissan:
+    '../images/cars/nissan/nissan-large-view2-mob-1x-min.png',
+  largeview2mob2nissan:
+    '../images/cars/nissan/nissan-large-view2-mob-2x-min.png',
+  view2desk1nissan: '../images/cars/nissan/nissan-view2-desk-1x-min.jpg',
+  view2desk2nissan: '../images/cars/nissan/nissan-view2-desk-2x-min.jpg',
+  view2mob1nissan: '../images/cars/nissan/nissan-view2-mob-1x-min.jpg',
+  view2mob2nissan: '../images/cars/nissan/nissan-view2-mob-2x-min.jpg',
+  largeview3desk1nissan:
+    '../images/cars/nissan/nissan-large-view3-desk-1x-min.jpg',
+  largeview3desk2nissan:
+    '../images/cars/nissan/nissan-large-view3-desk-2x-min.jpg',
+  largeview3mob1nissan:
+    '../images/cars/nissan/nissan-large-view3-mob-1x-min.jpg',
+  largeview3mob2nissan:
+    '../images/cars/nissan/nissan-large-view3-mob-2x-min.jpg',
+  view3desk1nissan: '../images/cars/nissan/nissan-view3-desk-1x-min.jpg',
+  view3desk2nissan: '../images/cars/nissan/nissan-view3-desk-2x-min.jpg',
+  view3mob1nissan: '../images/cars/nissan/nissan-view3-mob-1x-min.jpg',
+  view3mob2nissan: '../images/cars/nissan/nissan-view3-mob-2x-min.jpg',
+};
+
+// export
