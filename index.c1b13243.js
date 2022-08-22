7c3c5c23c2c5e27670b542317c9a779cab95446b
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-08-22 13:06:44 +0000

    Deploying to gh-pages from @ AnnaSobchenko/morent-car-rent@3b61f33119f5d66a2658724e15725e472b420945 🚀

---
+++ index.c1b13243.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerView:"auto",spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.c1b13243.js.map
---
+++ index.dcfb7699.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerView:"auto",spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.dcfb7699.js.map
